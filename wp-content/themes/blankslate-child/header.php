c963ddbe876ab45f1a677f4ac0ed4d7bafbc31eb
emcdesignerd <124219400+emcdesignerd@users.noreply.github.com>
2023-02-03 13:41:49 -0800

    Updated parent theme and added child theme

---
+++ wp-content/themes/blankslate-child/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+    <html <?php language_attributes(); ?>>
+
+        <head>
+            <meta name="facebook-domain-verification" content="dw5wrjx0uvh4pdv9gv0a7c571rg5j6" />
+            <meta charset="<?php bloginfo( 'charset' ); ?>" />
+            <meta name="viewport" content="width=device-width" />
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+            <?php wp_head(); ?>
+        </head>
+
+        <body <?php body_class(); ?>>
+            <div id="wrapper" class="hfeed">
+                <header id="header">
+
+                    <script>
+                        function myFunction() {
+                        var x = document.getElementById("menu_mob");
+                        if (x.style.display === "block") {
+                        x.style.display = "none";
+                        } else {
+                        x.style.display = "block";
+                        }
+                        }
+                    </script>
+
+                    <div id="main_header">
+                        <nav id="menu_mob">
+                            <a href="/" id="main_logo_mob_menu"></a>
+                            <a href="#" onClick="myFunction()" id="hamb_close">
+                            <div class="hamb_bar_close"></div>
+                            <a>
+                            <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+                        </nav>
+
+                        <a href="/" id="main_logo"></a>
+                        <div class="phone_menu">
+                            <a href="tel:1-800-555-5485" id="main_phone_mob">1-800-555-5485</a>
+                            <a href="tel:1-800-555-5485" id="main_phone">1-800-555-5485</a>
+                            <a href="#" onClick="myFunction()" id="hamb">
+                                <div class="hamb_bar"></div>
+                                <div class="hamb_bar"></div>
+                                <div class="hamb_bar"></div>
+                            <a>
+                            <nav id="menu">
+                                <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+                            </nav>
+                        </div>
+                    </div>
+                </header>
+                <div id="container">
--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -1,50 +1,10 @@
+</main>
+<?php get_sidebar(); ?>
 </div>
-<footer id="footer">
-
-
-<div class="footer_container">
-
-	<div class="footer_left">
-
-		<div class="footer_logo"></div>
-		<div class="footer_company">EMC ADVERTISING</div>
-		<div class="footer_address">2551 Warren Drive<br>Rocklin, CA 95677</div>
-		<a href="tel:1-800-555-5485" class="footer_phone">1-800-555-5485</a>
-
-		<nav id="footer_menu1">
-		<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-		</nav>
-
-		<nav id="footer_menu2">
-		<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-		</nav>
-
-	
-		
-		<div class="footer_social_container">
-			<a href="https://www.facebook.com/EMCAdvertising" target="_blank" title="Facebook"><i class="fab fa-facebook-f myfa"></i></a>
-			<a href="https://www.youtube.com//EMCAdvertising" target="_blank" title="YouTube"><i class="fab fa-youtube myfa"></i></a>
-			<a href="https://www.linkedin.com/company/emc-advertising" target="_blank" title="LinkedIn"><i class="fab fa-linkedin-in myfa"></i></a>
-			<a href="https://twitter.com/emcadvertising" target="_blank" title="Twitter"><i class="fab fa-twitter myfa"></i></a>
-			<a href="/blog/"><i class="fab fa-blogger-b myfa" title="Blog"></i></a>
-			<a href="https://goo.gl/maps/rYv3nGCe6o7JTdF78" target="_blank" title="Find us on a map"><i class="fas fa-map-marker-alt myfa"></i></a>
-		</div>
-
-		<div class="footer_disclaimer">©2022 EMC Advertising. All Rights Reserved.</div>
-		<a href="/privacy/" class="footer_privacy">PRIVACY POLICY</a>.
-
-	</div>
-
-	<div class="footer_right">
-		<div class="footer_lets">LET'S GET IN TOUCH!</div>
-		<?php gravity_form( 2, false, false, false, '', false ); ?>
-	</div>
-
-
-
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
-
-
 </footer>
 </div>
 <?php wp_footer(); ?>
